9544c7dfe70f2f28989cacb8f3a67c2d26cc451f
DipakVaidya <deepvaidyaraw@gmail.com>
2026-03-28 12:52:50 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log('Hello, Devops!');
+console.log('This is a simple Node.js application.');
+console.log('It is used to demonstrate a DevOps pipeline.');
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('Starting test ...');
+setTimeout(() => Console.log('Waiting 3 Seconds ..'), 3000);
+console.log('Test Completed!');
